10bcf60ef2ee9805743ab63e29a6bbee2450c5e8
TodoNikolov <tod_ant@yahoo.com>
2019-12-11 13:15:33 +0200

    Added CreateUser, ChangePassword, methods

---
+++ Sport/Sport.Models/Identity/UserWithRolesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sport.ViewModels.Identity
+{
+    public class UserWithRolesViewModel
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
--- Sport/Sport.Web/Controllers/IdentityController.cs
+++ Sport/Sport.Web/Controllers/IdentityController.cs
@@ -8,8 +8,10 @@
     using Sport.ViewModels.Identity;
     using Sport.Web.Infrastructure;
     using System.Linq;
+    using System.Threading.Tasks;
 
     [Authorize(Roles = GlobalConstants.AdministratorRole)]
+    [Route("identity")]
     public class IdentityController : Controller
     {
         private readonly SportDbContext db;
@@ -21,6 +23,7 @@
             this.userManager = userManager;
         }
 
+        [Route(nameof(All))]
         public IActionResult All()
         {
             var users = this.db
@@ -35,6 +38,111 @@
                 .ToList();
 
             return View(users);
+        }
+
+        [Route(nameof(Roles) + "/{id}")]
+        public async Task<IActionResult> Roles(string id)
+        {
+            var user = await this.userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var roles = await this.userManager.GetRolesAsync(user);
+
+            return View(new UserWithRolesViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Roles = roles
+            });
+
+        }
+
+        [Route(nameof(Create))]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [Route(nameof(Create))]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var result = await this.userManager.CreateAsync(new User
+            {
+                Email = model.Email,
+                UserName = model.Email,
+
+            }, model.Password);
+
+
+            if (result.Succeeded)
+            {
+                this.TempData["SuccessMessage"] = $"User with e-mail {model.Email} created";
+                return RedirectToAction(nameof(All));
+            }
+            else
+            {
+                AddModelErrors(result);
+                return View(model);
+            }
+        }
+
+        [Route(nameof(ChangePassword) + "/{id}")]
+        public async Task<IActionResult> ChangePassword(string id)
+        {
+            var user = await this.userManager.FindByIdAsync(id);
+
+            return View(new IdentityChangePasswordViewModel
+            {
+                Email = user.Email
+            });
+        }
+
+
+        [HttpPost]
+        [Route(nameof(ChangePassword) + "/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangePassword(string id, IdentityChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await this.userManager.FindByIdAsync(id);
+            var token = await this.userManager.GeneratePasswordResetTokenAsync(user);
+            var result = await this.userManager.ResetPasswordAsync(user, token, model.Password);
+
+
+            if (result.Succeeded)
+            {
+                this.TempData["SuccessMessage"] = $"Password changed for user {user.Email}";
+                return RedirectToAction(nameof(All));
+            }
+            else
+            {
+                AddModelErrors(result);
+                return View(model);
+            }
+        }
+
+        private void AddModelErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }
