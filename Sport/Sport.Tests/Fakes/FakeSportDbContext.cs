1418abfc5a30b1f07b60d91e26ea59ac38b63752
Todor <tod_ant@yahoo.com>
2020-02-12 21:37:37 +0200

    Added FakeSportDbContext.cs

---
+++ Sport/Sport.Tests/Fakes/FakeSportDbContext.cs
@@ -0,0 +1,29 @@
+﻿namespace Sport.Tests.Fakes
+{
+    using AutoMapper;
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+    using Sport.Profiles;
+    using System.Threading.Tasks;
+
+    public class FakeSportDbContext
+    {
+        public FakeSportDbContext(string name)
+        {
+            var options = new DbContextOptionsBuilder<SportDbContext>()
+               .UseInMemoryDatabase(name)
+               .Options;            
+
+             this.Data = new SportDbContext(options);
+        }
+
+
+        public SportDbContext Data { get; }
+
+        public async Task Add(params object[] data)
+        {
+            this.Data.AddRange(data);
+           await this.Data.SaveChangesAsync();
+        }
+    }
+}
---
+++ Sport/Sport.Tests/Services/MatchServiceTests.cs
@@ -0,0 +1,88 @@
+﻿namespace Sport.Tests
+{
+    using Profiles;
+    using Domain;
+    using Data;
+    using Services.Implementation;
+
+    using System.Threading.Tasks;
+    using Xunit;
+    using AutoMapper;
+    using Sport.Tests.Fakes;
+
+    public class MatchServiceTests
+    {
+        [Fact]
+        public async Task GetMatchTestShouldReturnTrueIfMatchExists()
+        {
+            //Arrange
+            const string databaseName = "MatchGetMatch";
+            var db = new FakeSportDbContext(databaseName);
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new DomainProfile());
+            });
+
+            var mapper = config.CreateMapper();
+
+
+            await db.Add(new Match
+            {
+                Id = 1,
+                FirstPlayer = new User
+                {
+                    FirstName = "Player1"
+                },
+                FirstPlayerId = "1",
+
+                SecondPlayer = new User
+                {
+                    FirstName = "Player2"
+                },
+                SecondPlayerId = "2",
+                Tournament = new Tournament
+                {
+                    Id = 1
+                }
+            },
+             new Match
+             {
+                 Id = 2,
+                 FirstPlayer = new User
+                 {
+                     FirstName = "Player3"
+                 },
+                 FirstPlayerId = "3",
+
+                 SecondPlayer = new User
+                 {
+                     FirstName = "Player4"
+                 },
+                 SecondPlayerId = "4",
+                 Tournament = new Tournament
+                 {
+                     Id = 2
+                 }
+             });
+
+            var matchService = new MatchService(db.Data, mapper);
+
+            //Act
+            var expectedResult = 2;
+            var actualResult = matchService.GetMatch(expectedResult);
+
+            //Assert
+            Assert.True(expectedResult == actualResult.Id);
+
+        }
+
+
+        //public Task GetAllTest()
+        //{
+        //    var options = new DbContextOptionsBuilder<SportDbContext>()
+        //       .UseInMemoryDatabase("MatchGetAll")
+        //       .Options;
+        //}
+    }
+}
