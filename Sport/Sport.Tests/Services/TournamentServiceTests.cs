4ec4db2be6309392a09b7d90d11225f1d0c32e2d
Todor <tod_ant@yahoo.com>
2020-02-17 00:21:01 +0200

    Added Xunit tests in TournamentServiceTests.cs

--- Sport/Sport.Tests/Services/TournamentServiceTests.cs
+++ Sport/Sport.Tests/Services/TournamentServiceTests.cs
@@ -14,6 +14,7 @@
     using System.Linq;
     using System;
     using Sport.ViewModels.Tournament;
+    using System.Collections.Generic;
 
     public class TournamentServiceTests
     {
@@ -45,7 +46,7 @@
             //Assert
             Assert.True(expectedTournamentsCount == actualTournamentsCount,
                 $"Expected count ot tournaments should be {expectedTournamentsCount} not {actualTournamentsCount}!");
-                    }
+        }
 
         [Fact]
         public async Task ByIdShouldReturnTournamentByGivenId()
@@ -72,16 +73,158 @@
 
             //Assert
             Assert.True(expectedTournamentId == actualTournament.Id);
-        }      
+        }
 
+
+        [Fact]
+        public async Task DeleteShouldRemoveTournamentById()
+        {
+            //Arrange
+            const string databaseName = "TournamentDelete";
+            var db = new FakeSportDbContext(databaseName);
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new DomainProfile());
+            });
+            var mapper = config.CreateMapper();
+
+            await AddTournaments(db);
+            //TODO userManager is not used
+
+            var userManager = GetMockUserManager();
+
+            ITournamentService service = new TournamentService(mapper, db.Data, userManager.Object);
+
+            //Act
+            var tournamentId = 1;
+            await service.Delete(tournamentId);
+
+            var tournament = GetTournament(db, tournamentId);
+
+            //Assert
+            Assert.True(tournament == null);
+        }
+
+
+
+        [Fact]
+        public async Task EditShouldEditTournamentByModel()
+        {
+            //Arrange
+            const string databaseName = "TournamentDelete";
+            var db = new FakeSportDbContext(databaseName);
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new DomainProfile());
+            });
+            var mapper = config.CreateMapper();
+
+            await AddTournaments(db);
+            //TODO userManager is not used
+
+            var userManager = GetMockUserManager();
+
+            ITournamentService service = new TournamentService(mapper, db.Data, userManager.Object);
+
+            //Act
+            int tournamentId = 1;
+            TournamentFormModel expectedTournament = new TournamentFormModel()
+            {
+                Id = tournamentId,
+                Name = "SofiaOpen",
+                Place = "Sofia"
+            };
+
+            await service.Edit(expectedTournament);
+
+            var actualTournament = GetTournament(db, tournamentId);
+
+            //Assert
+            Assert.Equal(expectedTournament.Name, actualTournament.Name);
+            Assert.Equal(expectedTournament.Place, actualTournament.Place);
+
+        }
+
+        [Fact]
+        public async Task EditShouldReturnTournamentNotfound()
+        {
+            //Arrange
+            const string databaseName = "TournamentDeleteNotfound";
+            var db = new FakeSportDbContext(databaseName);
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new DomainProfile());
+            });
+            var mapper = config.CreateMapper();
+
+            await AddTournaments(db);
+            //TODO userManager is not used
+
+            var userManager = GetMockUserManager();
+
+            ITournamentService service = new TournamentService(mapper, db.Data, userManager.Object);
+
+            //Act
+            int tournamentId = -1;
+            TournamentFormModel expectedTournament = new TournamentFormModel()
+            {
+                Id = tournamentId,
+                Name = "SofiaOpen",
+                Place = "Sofia"
+            };
+
+            await service.Edit(expectedTournament);
+
+            var actualTournament = GetTournament(db, tournamentId);
+
+            //Assert
+            Assert.True(actualTournament == null);
+
+        }
+
+        [Fact]
+        public async Task SigninShouldSignInCurrentUser()
+        {
+            //Arrange
+            const string databaseName = "TournamentDeleteNotfound";
+            var db = new FakeSportDbContext(databaseName);
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new DomainProfile());
+            });
+            var mapper = config.CreateMapper();
+
+            await AddTournaments(db);
+            //TODO userManager is not used
+
+            var userManager = GetMockUserManager();
+
+            ITournamentService service = new TournamentService(mapper, db.Data, userManager.Object);
+
+
+
+        }
+
+
+        private static Tournament GetTournament(FakeSportDbContext db, int tournamentId)
+        {
+            return db.Data.Tournaments.FirstOrDefault(t => t.Id == tournamentId);
+        }
 
         private static Task AddTournaments(FakeSportDbContext db)
         {
             return db.Add(
                 new Tournament()
                 {
-                    Id = 1
-                }, new Tournament()
+                    Id = 1,
+                    Name = "SofiaOpen",
+                    Place = "Sofia",                   
+                },
+                new Tournament()
                 {
                     Id = 2
                 });
